2fc37d4074b8b20c1a01e5ae16fa2470735137c5
ju yeong <87563115+ju-yeong@users.noreply.github.com>
2021-11-13 14:09:32 +0900

    지도 하

--- assets/js/main.js
+++ assets/js/main.js
@@ -318,11 +318,25 @@ init();
 
 var container = document.getElementById('map');
 var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    center: new kakao.maps.LatLng(37.56947246205971, 126.8350585819944),
     level: 3
 };
 
 var map = new kakao.maps.Map(container, options);
+
+// kakao map marker
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(37.56947246205971, 126.8350585819944); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
 
 // kakao map end
 
